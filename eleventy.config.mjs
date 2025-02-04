517a363c046269678f770542e7296cfc0e485e3f
Arnav2323 <arnavsharma3011@gmail.com>
2025-02-04 23:08:03 +0000

    should be good maybe?

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,19 +1,11 @@
-import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
-
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/styles");
     eleventyConfig.addPassthroughCopy("src/images");
-    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
     return {
         dir: {
             input: "src",
-            includes: "/_includes",
             output: "_site",
         },
     }
-}
-
-export const config = {
-    pathPrefix:  "/portsite11ty/",
 }
