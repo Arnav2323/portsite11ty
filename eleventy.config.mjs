662f0b55e65d15b2590eec4e7308a8ddddfddfcb
Arnav2323 <arnavsharma3011@gmail.com>
2025-02-05 07:14:01 +0000

    add htmlbase plugin

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,6 +1,9 @@
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/styles");
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
     return {
         dir: {
