9b44a5c39dfebd6722353886dea34eed4c157b82
Arnav2323 <arnavsharma3011@gmail.com>
2025-02-07 22:01:03 +0000

    minor: config file adjustments + js moved into scripts file

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,16 +1,22 @@
+// "use strict";
+
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/styles");
+    // Basic Config
+    eleventyConfig.setInputDirectory("src");
+    eleventyConfig.setIncludesDirectory("_includes");
+    eleventyConfig.setOutputDirectory("_site");
+
+    // Pass through copies
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/styles");
+    eleventyConfig.addPassthroughCopy("src/scripts");
+
+    // Extra
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
     return {
-        dir: {
-            input: "src",
-            includes: "_includes",
-            output: "_site",
-        },
-        pathPrefix: "/",
+        pathPrefix: "/"
     }
 }
